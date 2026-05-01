42b6c9a3010386ad9495419f627d3ecee730cea3
Bahram Malaekeh <Mithrandir21@users.noreply.github.com>
2026-05-01 18:39:32 +0100

    fix(#44): key window theming on LaunchedEffect (#58)

--- common/ui/src/main/java/pm/bam/gamedeals/common/ui/theme/Theme.kt
+++ common/ui/src/main/java/pm/bam/gamedeals/common/ui/theme/Theme.kt
@@ -11,6 +11,7 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.staticCompositionLocalOf
@@ -269,7 +270,7 @@ fun GameDealsTheme(
     }
     val view = LocalView.current
     if (!view.isInEditMode) {
-        SideEffect {
+        LaunchedEffect(colorScheme.primary, darkTheme) {
             val window = (view.context as Activity).window
             window.statusBarColor = colorScheme.primary.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
